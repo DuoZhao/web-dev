86b01cdce24672ac6ab18b87102d2ba1fcfecd62
duke <duz5@pitt.edu>
2022-02-28 20:04:45 -0500

    a5 JavaScript sp22

--- public/tuiter/NavigationSidebar/index.js
+++ public/tuiter/NavigationSidebar/index.js
@@ -4,12 +4,12 @@ const NavigationSidebar = () => {
             <a class="list-group-item disabled" href="/"><i class="fab fa-twitter" style="color: white"></i></a>
             <a href="../HomeScreen/home.html" class="list-group-item list-group-item-action" id="home"><i class="fa fa-home"></i> <div class="d-none d-xl-inline-block">Home</div></a>
             <a href="../ExploreScreen/explore.html" class="list-group-item list-group-item-action" id="explore"><i class="fa fa-hashtag"></i> <div class="d-none d-xl-inline-block">Explore</div></a>
-            <a href="../notifications.html" class="list-group-item list-group-item-action"><i class="fa fa-bell"></i> <div class="d-none d-xl-inline-block">Notifications</div></a>
-            <a href="../messages.html" class="list-group-item list-group-item-action"><i class="fa fa-envelope"></i> <div class="d-none d-xl-inline-block">Message</div></a>
-            <a href="../bookmarks.html" class="list-group-item list-group-item-action"><i class="fa fa-bookmark"></i> <div class="d-none d-xl-inline-block">Bookmarks</div></a>
-            <a href="../lists.html" class="list-group-item list-group-item-action"><i class="fa fa-list"></i> <div class="d-none d-xl-inline-block">Lists</div></a>
-            <a href="../profile.html" class="list-group-item list-group-item-action"><i class="fa fa-user"></i> <div class="d-none d-xl-inline-block">Profile</div></a>
-            <a href="#" class="list-group-item list-group-item-action"><i class="fa fa-caret-square-right"></i> <div class="d-none d-xl-inline-block">More</div></a>
+            <a href="#" class="list-group-item list-group-item-action wd-sb-action"><i class="fa fa-bell" ></i> <div class="d-none d-xl-inline-block">Notifications</div></a>
+            <a href="#" class="list-group-item list-group-item-action wd-sb-action"><i class="fa fa-envelope"></i> <div class="d-none d-xl-inline-block">Message</div></a>
+            <a href="#" class="list-group-item list-group-item-action wd-sb-action"><i class="fa fa-bookmark"></i> <div class="d-none d-xl-inline-block">Bookmarks</div></a>
+            <a href="#" class="list-group-item list-group-item-action wd-sb-action"><i class="fa fa-list"></i> <div class="d-none d-xl-inline-block">Lists</div></a>
+            <a href="#" class="list-group-item list-group-item-action wd-sb-action"><i class="fa fa-user"></i> <div class="d-none d-xl-inline-block">Profile</div></a>
+            <a href="#" class="list-group-item list-group-item-action wd-sb-action"><i class="fa fa-caret-square-right"></i> <div class="d-none d-xl-inline-block">More</div></a>
         </div>
         <div class="d-grid mt-2">
             <a href="tweet.html"
@@ -17,14 +17,17 @@ const NavigationSidebar = () => {
                 Tweet</a>
         </div>
         <script>
-
-            $( document ).ready(function() {
+            $(function() {
                 var url = window.location.href;
                 var token = url.split('/');
                 var index = token[token.length-1].split('.');
                 var name = "#" + index[0];
                 $(".list-group .list-group-item").removeClass("active");
                 $(name).addClass("active");
+                $('.wd-sb-action').click(function () {
+                   $('#home').removeClass("active");
+                   $('#explore').removeClass("active");
+                });
             });
         </script>
     `);
